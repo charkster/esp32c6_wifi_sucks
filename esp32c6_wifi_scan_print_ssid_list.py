03805ce8f907457e11f742b0a580ed5e3b3b24d3
charkster <31560568+charkster@users.noreply.github.com>
2025-05-29 20:11:39 -0700

    Update esp32c6_wifi_scan_print_ssid_list.py

--- esp32c6_wifi_scan_print_ssid_list.py
+++ esp32c6_wifi_scan_print_ssid_list.py
@@ -3,8 +3,10 @@ import network
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
+net_list = wlan.scan()
+net_list.sort(key=lambda x: -x[3])
 use_real_ssid_name = True
-for net in wlan.scan():
+for net in net_list:
     ssid_name = net[0].decode('ascii')
     if ssid_name:
         if (use_real_ssid_name):
